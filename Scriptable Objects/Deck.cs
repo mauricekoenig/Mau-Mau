5de2bcbe643aeaf72fe82a84243c0a4e4e0845a8
Maurice König <koenig_maurice@hotmail.com>
2021-07-24 04:48:06 +0200

    Stellt ein komplettes Set von Karten dar. Existiert nur 1x.

---
+++ Scriptable Objects/Deck.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MK.MauMau
+{
+    [CreateAssetMenu]
+    public sealed class Deck : ScriptableObject
+    {
+        public List<Card> Cards = new List<Card>();
+        public static readonly int Length = 32;
+    }
+}
