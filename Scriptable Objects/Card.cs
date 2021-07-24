b611b622320a853eb99982cf7b7c6d7a96d59a93
Maurice König <koenig_maurice@hotmail.com>
2021-07-24 04:47:18 +0200

    Scriptable Object wurde bereinigt.

---
+++ Scriptable Objects/Card.cs
@@ -0,0 +1,14 @@
+﻿
+
+using UnityEngine;
+
+namespace MK.MauMau
+{
+    [CreateAssetMenu]
+    public sealed class Card : ScriptableObject
+    {
+        public Value value;
+        public ColorProperty color;
+        public Sprite sprite;
+    }
+}
